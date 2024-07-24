05077ad330dd2a6edae469b801d757fb09b725ed
Mystjc <77810795+mystjc@users.noreply.github.com>
2024-07-24 11:59:14 -0400

    Adjust Color Scheme

--- .config/wezterm/colorscheme.lua
+++ .config/wezterm/colorscheme.lua
@@ -3,32 +3,32 @@ local module = {}
 
 function module.apply_colors(config)
 config.colors = {
-      foreground = '#c2ddff',
+      foreground = '#b3c8e1',
       background = '#242f3b',
       ansi = {
          '#293544', -- black
-         '#e14356', -- red
-         '#a0df6e', -- green
-         '#eabb56', -- yellow
-         '#639de0', -- blue
-         '#dc6fcc', -- magenta/purple
-         '#61bedc', -- cyan
-         '#b4ceed', -- white
+         '#de5c59', -- red
+         '#9dd470', -- green
+         '#dfb55d', -- yellow
+         '#6799d5', -- blue
+         '#c772d2', -- magenta/purple
+         '#67b6d1', -- cyan
+         '#aac2df', -- white
       },
       brights = {
          '#34465c', -- black
-         '#e75c66', -- red
+         '#ee6a65', -- red
          '#ace37e', -- green
          '#eec36a', -- yellow
          '#76a6e4', -- blue
-         '#e180d2', -- magenta/purple
+         '#d580e0', -- magenta/purple
          '#76c4e0', -- cyan
-         '#bdd3ef', -- white
+         '#c2ddff', -- white
       },
       cursor_fg = '#242f3b',
-      cursor_bg = '#c2ddff',
+      cursor_bg = '#b3c8e1',
       cursor_border = '#242f3b',
-      selection_fg = '#ffffff',
+      selection_fg = '#c2ddff',
       selection_bg = '#4586c4',
       split = '#4586c4',
       tab_bar = {
@@ -38,7 +38,7 @@ config.colors = {
             bg_color = '#4586c4',
          },
          inactive_tab = {
-            fg_color = '#bdd3ef',
+            fg_color = '#aac2df',
             bg_color = '#34465c',
          },
          inactive_tab_hover = {
@@ -46,7 +46,7 @@ config.colors = {
             bg_color = '#34465c',
          },
          new_tab = {
-            fg_color = '#bdd3ef',
+            fg_color = '#aac2df',
             bg_color = '#242f3b',
          },
          new_tab_hover = {
