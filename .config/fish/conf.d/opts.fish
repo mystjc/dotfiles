20be6ca023eec9875f663b1cc90b19c5d40f0f9a
Mystjc <77810795+mystjc@users.noreply.github.com>
2026-09-06 12:21:17 -0400

    Revamp MangoHud & update fish

--- .config/fish/conf.d/opts.fish
+++ .config/fish/conf.d/opts.fish
@@ -3,7 +3,7 @@ set fish_greeting
 
 # Syntax highlighting
 set fish_color_normal normal
-set fish_color_command blue
+set fish_color_command magenta
 set fish_color_builtin blue
 set fish_color_function magenta
 set fish_color_keyword magenta
